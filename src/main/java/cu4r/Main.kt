ad913127b13fddc6e5b9170e368ba75ae4737266
GEORGE <lws.sh302@gmail.com>
2018-04-20 13:34:13 +0900

    feature: 1st commit

---
+++ src/main/java/cu4r/Main.kt
@@ -0,0 +1,25 @@
+package cu4r
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class Main : Application() {
+
+    @Throws(Exception::class)
+    override fun start(primaryStage: Stage) {
+        val root = FXMLLoader.load<Parent>(javaClass.getResource("/main.fxml"))
+        primaryStage.title = "csvutil4research"
+        primaryStage.scene = Scene(root)
+        primaryStage.show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(Main::class.java)
+        }
+    }
+}
---
+++ src/main/java/cu4r/MainController.kt
@@ -0,0 +1,12 @@
+package cu4r
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class MainController : Initializable {
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+
+    }
+
+}
