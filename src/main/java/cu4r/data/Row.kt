505f1a6741dbf850b8ee6ab4ab7200f208b4453d
GEORGE <lws.sh302@gmail.com>
2018-05-01 13:30:23 +0900

    fix: セル先頭の空白を無視するように修正

--- src/main/java/cu4r/data/Row.kt
+++ src/main/java/cu4r/data/Row.kt
@@ -2,11 +2,12 @@ package cu4r.data
 
 import javafx.beans.property.SimpleStringProperty
 import javafx.beans.property.StringProperty
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
 
 class Row(
         vararg elements: String
 ) {
-    val elements: MutableList<StringProperty> = elements.map { SimpleStringProperty(it) }.toMutableList()
+    val elements: MutableList<StringProperty> = elements.map {
+        val element = it.replace(Regex("""^ """), "")
+        SimpleStringProperty(element)
+    }.toMutableList()
 }
